e2baa7035c1eea014e7510c6d57e8356d785bbf9
glattercj <glattercj@gmail.com>
2023-11-22 14:09:19 -0700

    Use xz with extreme compression in ramdisk plugin.

--- vmdb/plugins/ramdisk_plugin.py
+++ vmdb/plugins/ramdisk_plugin.py
@@ -42,7 +42,7 @@ class RamdiskStepRunner(vmdb.StepRunnerInterface):
         kernel = '{}.kernel'.format(fname)
         cp_kernel = 'find boot/ -type f -name "vmlinu*" -exec cp {{}} {} \;'.format(kernel)
         cmd = ("cd {} && find . -print0 | cpio --quiet --null -ov --format=newc | "
-               "pigz -9 > {} && {}").format(mount_point, initrd, cp_kernel)
+               "xz -9e > {} && {}").format(mount_point, initrd, cp_kernel)
         fd, path = tempfile.mkstemp()
         with open(fd, 'w') as file_:
             file_.write(cmd)
